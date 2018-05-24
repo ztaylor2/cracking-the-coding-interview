c9650004b9b8f9990798ef6d0486a1d89f12ff95
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-24 15:55:16 -0700

    1.1 part two passing test for returning true.

--- chapter_1/test_1_1.py
+++ chapter_1/test_1_1.py
@@ -17,3 +17,9 @@ def test_part2_returns_false():
     """Test that the method that doesn't use data structures returns flase."""
     from CTCI_1_1 import unique_charactors_no_data_structure
     assert unique_charactors_no_data_structure('aa') is False
+
+
+def test_part2_returns_ture():
+    """Test that method returns true."""
+    from CTCI_1_1 import unique_charactors_no_data_structure
+    assert unique_charactors_no_data_structure('ab') is True
