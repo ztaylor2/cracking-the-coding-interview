0004bba6dd6e33d948a20b45f5447113dad77994
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-24 15:44:30 -0700

    1.1 passing test for returning true.

--- chapter_1/test_1_1.py
+++ chapter_1/test_1_1.py
@@ -5,3 +5,9 @@ def test_returns_false():
     """Test that the method returns false when it should."""
     from CTCI_1_1 import unique_charactors
     assert unique_charactors('aa') is False
+
+
+def test_returns_true():
+    """Test that the method returns true when it should."""
+    from CTCI_1_1 import unique_charactors
+    assert unique_charactors('ab') is True
