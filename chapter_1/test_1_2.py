38eef7f4231078925f208d5ef2bd9e7bd6a24150
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-28 12:19:01 -0700

    1.2 is passing tests for true and false.

--- chapter_1/test_1_2.py
+++ chapter_1/test_1_2.py
@@ -1,1 +1,13 @@
 """Test problem 1.2."""
+
+
+def test_permutations_true():
+    """Test that the permutaiton method returns true correctly."""
+    from CTCI_1_2 import is_permutation
+    assert is_permutation('abcdefg', 'gfedcba') is True
+
+
+def test_permutation_false():
+    """Test that a permutation returns false correctly."""
+    from CTCI_1_2 import is_permutation
+    assert is_permutation('string_1', 'string_2') is False
