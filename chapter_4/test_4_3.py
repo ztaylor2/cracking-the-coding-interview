a122a1ff78183273b3390a62952b7be6f1ecc16e
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-31 17:12:08 -0700

    wrote test for problem 1.5, not passing for replacing a charactor because it counts one extra.

--- chapter_4/test_4_3.py
+++ chapter_4/test_4_3.py
@@ -5,15 +5,15 @@ sys.path.insert(0, '/Users/zt/programming/interviews/interview-prep/cracking-the
 from bst import BinarySearchTree, Node
 
 
-def test_list_of_depths():
-    """Test list of depths returns correctly."""
-    from CTCI_4_3 import list_of_depths
-    bst = BinarySearchTree()
-    bst.root = Node(5)
-    bst.root.left = Node(3)
-    bst.root.right = Node(7)
-    bst.root.left.left = Node(2)
-    bst.root.left.right = Node(4)
-    bst.root.right.left = Node(6)
-    bst.root.right.right = Node(8)
-    assert list_of_depths(bst) == [[5], [3, 7], [2, 4, 6, 8]]
+# def test_list_of_depths():
+#     """Test list of depths returns correctly."""
+#     from CTCI_4_3 import list_of_depths
+#     bst = BinarySearchTree()
+#     bst.root = Node(5)
+#     bst.root.left = Node(3)
+#     bst.root.right = Node(7)
+#     bst.root.left.left = Node(2)
+#     bst.root.left.right = Node(4)
+#     bst.root.right.left = Node(6)
+#     bst.root.right.right = Node(8)
+#     assert list_of_depths(bst) == [[5], [3, 7], [2, 4, 6, 8]]
