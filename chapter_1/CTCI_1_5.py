7cbe41fdc460556ce7378a62f233dfbf281488f1
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-31 17:18:04 -0700

    wrote more readable solution to problem 1.5.

--- chapter_1/CTCI_1_5.py
+++ chapter_1/CTCI_1_5.py
@@ -12,6 +12,8 @@ pale, bale -> true
 pale, bake -> false
 """
 
+# bruit force solution
+
 
 def one_away(string_1, string_2):
     """Determine if the two strings are one edit away."""
@@ -42,3 +44,41 @@ def one_away(string_1, string_2):
     if num_edits > 1:
         return False
     return True
+
+# readable solution:
+
+
+# def one_away(s1, s2):
+#     '''Check if a string can converted to another string with a single edit'''
+#     if len(s1) == len(s2):
+#         return one_edit_replace(s1, s2)
+#     elif len(s1) + 1 == len(s2):
+#         return one_edit_insert(s1, s2)
+#     elif len(s1) - 1 == len(s2):
+#         return one_edit_insert(s2, s1)
+#     return False
+
+
+# def one_edit_replace(s1, s2):
+#     edited = False
+#     for c1, c2 in zip(s1, s2):
+#         if c1 != c2:
+#             if edited:
+#                 return False
+#             edited = True
+#     return True
+
+
+# def one_edit_insert(s1, s2):
+#     edited = False
+#     i, j = 0, 0
+#     while i < len(s1) and j < len(s2):
+#         if s1[i] != s2[j]:
+#             if edited:
+#                 return False
+#             edited = True
+#             j += 1
+#         else:
+#             i += 1
+#             j += 1
+#     return True
